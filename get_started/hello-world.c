63e8e22f20d0103cfc16cd6f69ae37bbee42d6ae
Solomon Xie <solomonxiewise@gmail.com>
2018-11-16 01:45:35 +0800

    First C script: Hello world!

---
+++ get_started/hello-world.c
@@ -0,0 +1,19 @@
+/* 
+ * How to run:
+ *     1.Compile: (almost every *nix OS has "gcc" pre-installed)
+ *         $ gcc hello-world.c
+ *     2.Run: (similar to bash script)
+ *         $ ./a.out
+ * */
+
+/* Include the Standard I/O library */
+#include <stdio.h>
+
+/* Main entry function */
+int main(){
+
+    printf("Hello, world!");
+
+    /* Return is a must to the function */ 
+    return 0;
+}
