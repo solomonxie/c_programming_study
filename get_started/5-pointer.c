ef3d397a025f843603a514f4e69a239a35c7574e
Solomon Xie <solomonxiewise@gmail.com>
2018-11-17 15:19:46 +0800

    null pointer

--- get_started/5-pointer.c
+++ get_started/5-pointer.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 
-int main(){
 
+void null_pointer(){
+    int *ptr = NULL;
+
+    printf("Null pointer: %p", ptr);
+}
+
+
+void pointer_basics(){
+    /*
+     * "%p" is format for pointer
+     * "&" will return the Mem-address of a variable
+    */
     int height = 20;
-    int *addr = &height;  // Declare a pointer: notice the * and &
 
-    // "%p" is format for pointer
-    // "&" will return the Mem-address of a variable
+    int *addr = &height;  // Declare a pointer: notice the * and &
+    
+    // Variable's address: 
     printf("%p\n", &height);
+
+    // V's address stored in a pointer: 
     printf("%p\n", addr);
 
     // ??
     printf("%d\n", *addr);
+}
+
+
+int main(){
+
+    //pointer_basics();
+    null_pointer();
 
     return 0;
 }
+
